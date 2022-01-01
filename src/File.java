28205449808f4b3a7ad546620239c6990a2bbf1b
TomerGrinberg1 <81254017+TomerGrinberg1@users.noreply.github.com>
2022-01-01 17:39:48 +0200

    Update File.java
    documentation

--- src/File.java
+++ src/File.java
@@ -2,6 +2,13 @@ public class File extends StorageItem
 {
  String ending;
  String content;
+ 
+ 
+/**
+     * initializes the name, end of file and the contents of the file
+     * @param name: the name of the file
+     * @param ending: the name of the end of the file
+     */
  public File (String name, String ending)
  {
      super(name);
@@ -9,6 +16,10 @@ public class File extends StorageItem
      this.ending=ending;
 
  }
+ /**
+     * create the name of the file on format of "name.ext"
+     * @return : the name of the file on format of "name.ext"
+     */
  public String getName()
  {
      return this.name+"."+this.ending;
@@ -18,17 +29,28 @@ public class File extends StorageItem
  public String getEnding(){
      return this.ending;
 }
-
+ /**
+     * checking if two files have the same nama and ending
+     * @param item: the file to compare with
+     */
 
     public boolean isEqual(File item) {
         return super.isEqual(item)&&(this.ending.equals(item.ending)) ;
     }
 
+ /**
+     * add contents to the file contents
+     * @param contentToAdd: the contents to add to the file contents
+     */
     void addContent(String contentToAdd)
    {
        this.content+=contentToAdd;
    }
 
+/**
+     * get the size of the file contents
+     * @return : the size of the file contents
+     */
     @Override
     public int getSize() {
        if(content==null)
@@ -41,7 +63,10 @@ public class File extends StorageItem
         System.out.println(this.content);
 
     }
-
+ /**
+     * print tree of File as requested
+     * @param sortBy: the method according to print the tree
+     */
     public void printTree(SortingField sortBy) {
         System.out.println(this.getName());
 
