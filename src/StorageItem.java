7d352f8518c97a075c820c84e660b524e3a67588
yogev-namir <81289035+yogev-namir@users.noreply.github.com>
2022-01-04 18:54:39 +0200

    Update StorageItem.java

--- src/StorageItem.java
+++ src/StorageItem.java
@@ -3,20 +3,17 @@ import java.util.Calendar;
 import java.util.Date;
 
 public abstract class StorageItem {
-
-
-    String name;
-    Date date;
-    int size;
-/**
-     * initializes the the name of the storage item and random production date
+    protected String name;
+    protected Date date;
+    /**
+     * initializes the name of the storage item and random production date
      * * @param name: the name of the storage item
      */
     public StorageItem(String name) {
         this.name = name;
         this.date = random();
     }
-/**
+    /**
      * get the  creation Date of the storage item
      * @return : the  creation Date of the storage item
      */
@@ -27,32 +24,38 @@ public abstract class StorageItem {
      * get the name of the storage item
      * @return : the name of the storage item
      */
-    public String getName()
-    {
+    public String getName() {
         return this.name;
-
     }
-/**
+    /**
+     * @return : the name of the storage in lower case letters
+     */
+    public String getNameIgnoreCase() {
+        return this.name.toUpperCase().toLowerCase();
+    }
+    /**
      * get the size of the storage item
      * @return : the size of the storage item
      */
     public abstract int getSize();
-
-    public void printTree(SortingField sortBy) {
-
-    }
+    /**
+     * @param sortBy: storageItem's enum
+     * prints the files tree
+     */
+    public abstract void printTree(SortingField sortBy);
+    /**
+     * abstract method, compare to items
+     * @return : the true if they are equals
+     */
     public boolean isEqual(StorageItem item)
     {
         return (this.name.equals(item.name));
-
-
     }
- /**
+    /**
      * choose random date between 01.01.2017 - 31.12.2022
-     * @return: the random date
+     * @return : random time in range
      */
-
-  public Timestamp random() {
+    public Timestamp random() {
         final Date minDate, maxDate;
         Timestamp rndDate;
         long range;
@@ -60,21 +63,8 @@ public abstract class StorageItem {
         maxDate = new Date(122, Calendar.DECEMBER, 31, 23, 59, 59);
         range = maxDate.getTime() - minDate.getTime() ;
 
-        rndDate = new Timestamp(minDate.getTime() + Math.abs(Main.rnd.nextLong() % range));
+        rndDate = new Timestamp(minDate.getTime() +
+                Math.abs(Main.rnd.nextLong() % range));
         return rndDate;
-
     }
-
-
 }
-
-
-
-
-
-
-
-
-
-
-
