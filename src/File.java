70fc774a1d6698e6005ec94d9a689b7bf5049225
yogev-namir <81289035+yogev-namir@users.noreply.github.com>
2022-01-04 18:52:02 +0200

    Update File.java

--- src/File.java
+++ src/File.java
@@ -1,69 +1,73 @@
 public class File extends StorageItem
 {
- String ending;
- String content;
- 
- 
-/**
+     protected String ending;
+     protected String content;
+     /**
      * initializes the name, end of file and the contents of the file
      * @param name: the name of the file
      * @param ending: the name of the end of the file
      */
- public File (String name, String ending)
- {
-     super(name);
-     this.content=null;
-     this.ending=ending;
+    public File (String name, String ending) {
+        super(name);
+        this.content="";
+        this.ending=ending;
 
- }
- /**
+    }
+    /**
      * create the name of the file on format of "name.ext"
      * @return : the name of the file on format of "name.ext"
      */
- public String getName()
- {
-     return this.name+"."+this.ending;
+    public String getName() {
+         return this.name+"."+this.ending;
 
- }
+    }
+    /**
+     * @return file's name in lower case letters
+     */
+    public String getNameIgnoreCase() {
+         return super.getNameIgnoreCase()+"."+this.ending.toLowerCase();
+    }
+    /**
+     * @return file's ending type
+     */
+    public String getEnding(){
+        return this.ending;
+    }
 
- public String getEnding(){
-     return this.ending;
-}
- /**
+    /**
      * checking if two files have the same nama and ending
      * @param item: the file to compare with
      */
-
     public boolean isEqual(File item) {
-        return super.isEqual(item)&&(this.ending.equals(item.ending)) ;
+        return super.isEqual(item)&&(this.getEnding().equals(item.ending)) ;
     }
-
- /**
+    /**
      * add contents to the file contents
      * @param contentToAdd: the contents to add to the file contents
      */
-    void addContent(String contentToAdd)
-   {
+    void addContent(String contentToAdd) {
        this.content+=contentToAdd;
    }
-
-/**
+    /**
      * get the size of the file contents
      * @return : the size of the file contents
      */
     @Override
     public int getSize() {
-       if(content==null)
-           return 0;
-        return this.content.length();
+
+        return (this.content.length());
     }
-     public  void printContent()
+    /**
+     * prints file's name, size and last modified date
+     */
+     public void printContent()
     {
-        System.out.println(getName()+" Size: " +getSize()+"MB "+ "Created:"+this.date);
+        System.out.println(getName()+" Size: " +getSize()+"MB "+
+                "Created: "+this.date);
         System.out.println(this.content);
 
     }
- /**
+    /**
      * print tree of File as requested
      * @param sortBy: the method according to print the tree
      */
@@ -71,5 +75,4 @@ public class File extends StorageItem
         System.out.println(this.getName());
 
     }
-
 }
