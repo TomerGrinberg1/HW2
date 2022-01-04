bbec1379cd7a9c95dba39894ec609034558b6ee4
yogev-namir <81289035+yogev-namir@users.noreply.github.com>
2022-01-04 18:51:40 +0200

    Update Clock.java

--- src/Clock.java
+++ src/Clock.java
@@ -5,17 +5,34 @@ public class Clock {
      static final int MINUTE_MAX_VALUE = 59;
      static final int HOUR_DEFAULT_VALUE = 0;
      static final int MINUTE_DEFAULT_VALUE = 0;
-      int hour;
-      int minute;
-
+     static final int MILITARY_TIME_FORMAT = 10;
+     protected int hour;
+     protected int minute;
+    /**
+     * initializes a clock
+     * @param hour: the hour of the clock
+       @param minute: the minute of the clock
+     */
     public Clock(int hour, int minute) {
         setHour(hour);
         setMinute(minute);
     }
 
+    /**
+     * checks if the time is in range limit
+     * @param num the value that we check is it's in range
+     * @param lowerBound min value
+     * @param upperBound max value
+     * @return true if in range ,else return false
+     */
     public boolean inRange(int num, int lowerBound, int upperBound) {
         return num >= lowerBound && num <= upperBound;
     }
+
+    /**
+     * set hour for the clock
+     * @param hour required hour value to be set
+     */
     public void setHour(int hour) {
         if (!inRange(hour, HOUR_MIN_VALUE, HOUR_MAX_VALUE)) {
             this.hour = HOUR_DEFAULT_VALUE;
@@ -23,6 +40,11 @@ public class Clock {
             this.hour = hour;
         }
     }
+
+    /**
+     * set minutes for the clock
+     * @param minute required minutes value to be set
+     */
     public void setMinute(int minute) {
         if (!inRange(minute, MINUTE_MIN_VALUE, MINUTE_MAX_VALUE)) {
             this.minute = MINUTE_DEFAULT_VALUE;
@@ -30,44 +52,44 @@ public class Clock {
             this.minute = minute;
         }
     }
+
+    /**
+     * checks if the clock is equal to other clock
+     * @param other second clock or accurate clock object
+     * @return true if the clocks are equal, else return false
+     */
     @Override
     public boolean equals(Object other) {
-        if (!(other instanceof Clock) || (other instanceof AccurateClock)) {
+        if (!(other instanceof Clock) || this.hashCode()!=other.hashCode()) {
             return false;
         }
         Clock otherClock = (Clock) other;
         return this.hour == otherClock.hour && this.minute == otherClock.minute;
     }
+
+    /**
+     * generate a hashcode for a clock
+     * @return the hashcode
+     */
     @Override
     public int hashCode() {
-        return 60 * hour + minute;
+        return 60 * hour + minute-1;
     }
 
+    /**
+     * print a clock as required(military time)
+     * @return a string contains the time
+     */
     @Override
     public String toString() {
         String hours = Integer.valueOf(this.hour).toString(),
                minutes = Integer.valueOf(this.minute).toString();
 
-        if(this.hour<10)
+        if(this.hour<MILITARY_TIME_FORMAT)
             hours = "0" + hours;
-        if(this.minute<10)
+        if(this.minute<MILITARY_TIME_FORMAT)
             minutes = "0" +minutes;
 
         return hours + ":" + minutes;
-
-
-
-
-        /*
-        if(this.hour<10 && this.minute<10)
-            return "0"+this.hour+":0"+this.minute;
-        if(this.hour<10)
-        return "0"+this.hour+":"+this.minute;
-        if(this.minute<10)
-            return this.hour+":0"+this.minute;
-        return this.hour+":"+this.minute;
-
-        */
     }
-
 }
